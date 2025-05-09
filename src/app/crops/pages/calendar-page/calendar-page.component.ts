1ed5e5d684a1df348aecc161fef9b8ccb6c6957e
Anatoly69420 <U202211813@upc.edu.pe>
2025-05-09 04:31:27 -0500

    feat: add validations to calendar page

--- src/app/crops/pages/calendar-page/calendar-page.component.ts
+++ src/app/crops/pages/calendar-page/calendar-page.component.ts
@@ -1,29 +1,47 @@
-import { Component } from '@angular/core';
-import {CalendarComponent} from '../../components/calendar/calendar.component';
-import {ActivatedRoute} from '@angular/router';
-import {ActivityFormComponent} from '../../components/activity-form/activity-form.component';
-import {NgIf} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {CommonModule, NgIf} from '@angular/common';
+import { CalendarComponent } from '../../components/calendar/calendar.component';
+import { ActivityFormComponent } from '../../components/activity-form/activity-form.component';
+import { ActivityDetailsComponent } from '../../components/activity-details/activity-details.component';
+import {RoleService} from '../../../iam/services/role.service';
 
 @Component({
   selector: 'app-calendar-page',
   standalone: true,
   imports: [
+    CommonModule,
     CalendarComponent,
     ActivityFormComponent,
+    ActivityDetailsComponent,
     NgIf
   ],
   templateUrl: './calendar-page.component.html',
   styleUrl: './calendar-page.component.css'
 })
-export class CalendarPageComponent {
+export class CalendarPageComponent implements OnInit {
   cropId: string = '';
   selectedDate: Date | null = null;
+  role: string = 'farmer';
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private roleService: RoleService
+  ) {
     this.cropId = this.route.snapshot.paramMap.get('id') || '';
+  }
+
+  ngOnInit(): void {
+    this.roleService.getRole$().subscribe((role: string) => {
+      this.role = role;
+    });
   }
 
   handleDateChange(date: Date) {
     this.selectedDate = date;
+  }
+
+  closeForm() {
+    this.selectedDate = null;
   }
 }
