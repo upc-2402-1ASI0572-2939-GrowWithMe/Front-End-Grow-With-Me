7dff4a46d329c05157dc8b912728d65f073e71dc
Anatoly69420 <U202211813@upc.edu.pe>
2025-05-08 20:27:00 -0500

    feat: add route for the activities calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSidenav, MatSidenavContainer} from '@angular/material/sidenav';
+import {SidebarComponent} from './public/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenav, MatSidenavContainer, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
 import {TableCropsComponent} from './crops/pages/table-crops/table-crops.component';
+import {CalendarPageComponent} from './crops/pages/calendar-page/calendar-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'crops', component: TableCropsComponent},
+  { path: 'crops', component: TableCropsComponent },
+  { path: 'crops/:id/calendar', component: CalendarPageComponent },
 ];
