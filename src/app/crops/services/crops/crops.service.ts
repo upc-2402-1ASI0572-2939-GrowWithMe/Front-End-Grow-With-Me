08af7d4d7744df33214142bb542934d721a923ef
Anatoly69420 <U202211813@upc.edu.pe>
2025-05-08 20:27:33 -0500

    feat: add roles functionality to test the different sidebars

--- src/app/crops/services/crops/crops.service.ts
+++ src/app/crops/services/crops/crops.service.ts
@@ -1,34 +1,21 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Crop} from '../../models/crop.entity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CropsService {
+  private apiUrl = 'http://localhost:3000/crops';
 
-  // Simulación de datos de cultivos
-  private cropsData = [
-    { id: 1, name: 'Cultivo 1', type: 'Hortalizas', area: '120 m²' },
-    { id: 2, name: 'Cultivo 2', type: 'Frutas', area: '200 m²' },
-    { id: 3, name: 'Cultivo 3', type: 'Verduras', area: '150 m²' },
-    { id: 4, name: 'Cultivo 4', type: 'Hortalizas', area: '100 m²' }
-  ];
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
-
-  // Simula la obtención de cultivos desde una API
-  getCrops(): Observable<any[]> {
-    return of(this.cropsData);  // Utiliza 'of' para devolver los datos simulados como un observable
+  getCrops(): Observable<Crop[]> {
+    return this.http.get<Crop[]>(this.apiUrl);
   }
 
-
-  // Método simulado para eliminar un cultivo
-  deleteCrop(id: number): Observable<any> {
-    const index = this.cropsData.findIndex(c => c.id === id);
-    if (index !== -1) {
-      this.cropsData.splice(index, 1);
-      return of({ message: 'Cultivo eliminado' });
-    }
-    return of(null);
+  deleteCrop(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
