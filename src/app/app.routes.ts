be6f3b0142cff521a2b88c9851a157d4567b4276
Anatoly69420 <U202211813@upc.edu.pe>
2025-05-10 00:05:24 -0500

    feat: add consultor route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
 import {TableCropsComponent} from './crops/pages/table-crops/table-crops.component';
 import {CalendarPageComponent} from './crops/pages/calendar-page/calendar-page.component';
+import {MyConsultantPageComponent} from './consultations/pages/my-consultant-page/my-consultant-page.component';
+import {HistoryViewComponent} from './consultations/pages/history-view/history-view.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'crops', component: TableCropsComponent },
   { path: 'crops/:id/calendar', component: CalendarPageComponent },
+  { path: 'consultor', component: MyConsultantPageComponent },
+  { path: 'consultor/:id/history', component: HistoryViewComponent },
 ];
