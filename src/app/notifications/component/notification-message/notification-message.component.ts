70cc49c38ee87824ef3398ec974e972f47bc839d
carlos <U20201C794@upc.edu.pe>
2025-06-15 18:55:58 -0500

    feat:notification icon

---
+++ src/app/notifications/component/notification-message/notification-message.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-message',
+  imports: [],
+  templateUrl: './notification-message.component.html',
+  styleUrl: './notification-message.component.css'
+})
+export class NotificationMessageComponent {
+
+}
