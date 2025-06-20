53f3e2f7804ed3f691bc65d4f87f1231c2773212
carlos <U20201C794@upc.edu.pe>
2025-06-19 23:08:22 -0500

    feat:notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,29 @@ import {MatSidenav, MatSidenavContainer} from '@angular/material/sidenav';
 import {SidebarComponent} from './public/components/sidebar/sidebar.component';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common'; // <-- ESTA ES LA CLAVE
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenav, MatSidenavContainer, SidebarComponent, MatIcon, MatIconButton],
+  imports: [RouterOutlet, MatSidenav, MatSidenavContainer, SidebarComponent, MatIcon, MatIconButton,MatMenuModule,MatIconModule,MatButtonModule, MatListModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'FrontGWM';
+   notifications = [
+    'Alert: High temperature in Zone 1',
+    'Humidity level dropped in Zone 3',
+    'Fertilizer low in tank A',
+    'New report generated',
+    'Maintenance task scheduled',
+    'Consultant message: Please check your sensors',
+    'Weather warning in your area',
+    'New tips available for soil treatment'
+  ];
 }
